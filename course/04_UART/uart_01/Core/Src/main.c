7e772f3003fb00bce9470f21a0ad9f1dd92b4d39
Natalia Pluta <natalia0pluta@gmail.com>
2022-03-13 12:00:17 +0100

    04_UART: multiline sending

--- course/04_UART/uart_01/Core/Src/main.c
+++ course/04_UART/uart_01/Core/Src/main.c
@@ -33,6 +33,7 @@
 
 /* Private define ------------------------------------------------------------*/
 /* USER CODE BEGIN PD */
+#define LINE_MAX_LENGTH 80
 /* USER CODE END PD */
 
 /* Private macro -------------------------------------------------------------*/
@@ -44,7 +45,8 @@
 UART_HandleTypeDef huart2;
 
 /* USER CODE BEGIN PV */
-
+static char line_buffer[LINE_MAX_LENGTH + 1];
+static uint32_t line_length;
 /* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
@@ -53,8 +55,9 @@ void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
 
 static void MX_USART2_UART_Init(void);
-/* USER CODE BEGIN PFP */
 
+/* USER CODE BEGIN PFP */
+void line_append(uint8_t value);
 /* USER CODE END PFP */
 
 /* Private user code ---------------------------------------------------------*/
@@ -103,160 +106,179 @@ int main(void)
 
         /* USER CODE BEGIN 3 */
         uint8_t value;
-        if (HAL_UART_Receive(&huart2, &value, 1, 2000) == HAL_OK)
+        if (HAL_UART_Receive(&huart2, &value, 1, 0) == HAL_OK)
         {
-            printf("Odebrano: %c\n", value);
-        }
-        else
-        {
-            printf(".");
-            fflush(stdout);
+            line_append(value);
         }
         /* USER CODE END 3 */
     }
 }
+
 /**
   * @brief System Clock Configuration
   * @retval None
   */
-    void SystemClock_Config(void)
+void SystemClock_Config(void)
+{
+    RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+    RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+    RCC_PeriphCLKInitTypeDef PeriphClkInit = {0};
+
+    /** Initializes the RCC Oscillators according to the specified parameters
+    * in the RCC_OscInitTypeDef structure.
+    */
+    RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_MSI;
+    RCC_OscInitStruct.MSIState = RCC_MSI_ON;
+    RCC_OscInitStruct.MSICalibrationValue = 0;
+    RCC_OscInitStruct.MSIClockRange = RCC_MSIRANGE_6;
+    RCC_OscInitStruct.PLL.PLLState = RCC_PLL_NONE;
+    if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
     {
-        RCC_OscInitTypeDef RCC_OscInitStruct = {0};
-        RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
-        RCC_PeriphCLKInitTypeDef PeriphClkInit = {0};
-
-        /** Initializes the RCC Oscillators according to the specified parameters
-        * in the RCC_OscInitTypeDef structure.
-        */
-        RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_MSI;
-        RCC_OscInitStruct.MSIState = RCC_MSI_ON;
-        RCC_OscInitStruct.MSICalibrationValue = 0;
-        RCC_OscInitStruct.MSIClockRange = RCC_MSIRANGE_6;
-        RCC_OscInitStruct.PLL.PLLState = RCC_PLL_NONE;
-        if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
-        {
-            Error_Handler();
-        }
-        /** Initializes the CPU, AHB and APB buses clocks
-        */
-        RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK
-                                      | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2;
-        RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_MSI;
-        RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
-        RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
-        RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
-
-        if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_0) != HAL_OK)
-        {
-            Error_Handler();
-        }
-        PeriphClkInit.PeriphClockSelection = RCC_PERIPHCLK_USART2;
-        PeriphClkInit.Usart2ClockSelection = RCC_USART2CLKSOURCE_PCLK1;
-        if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInit) != HAL_OK)
-        {
-            Error_Handler();
-        }
-        /** Configure the main internal regulator output voltage
-        */
-        if (HAL_PWREx_ControlVoltageScaling(PWR_REGULATOR_VOLTAGE_SCALE1) != HAL_OK)
-        {
-            Error_Handler();
-        }
+        Error_Handler();
     }
+    /** Initializes the CPU, AHB and APB buses clocks
+    */
+    RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK
+                                  | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2;
+    RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_MSI;
+    RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+    RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
+    RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+
+    if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_0) != HAL_OK)
+    {
+        Error_Handler();
+    }
+    PeriphClkInit.PeriphClockSelection = RCC_PERIPHCLK_USART2;
+    PeriphClkInit.Usart2ClockSelection = RCC_USART2CLKSOURCE_PCLK1;
+    if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInit) != HAL_OK)
+    {
+        Error_Handler();
+    }
+    /** Configure the main internal regulator output voltage
+    */
+    if (HAL_PWREx_ControlVoltageScaling(PWR_REGULATOR_VOLTAGE_SCALE1) != HAL_OK)
+    {
+        Error_Handler();
+    }
+}
 
 /**
   * @brief USART2 Initialization Function
   * @param None
   * @retval None
   */
-    static void MX_USART2_UART_Init(void)
+static void MX_USART2_UART_Init(void)
+{
+
+    /* USER CODE BEGIN USART2_Init 0 */
+
+    /* USER CODE END USART2_Init 0 */
+
+    /* USER CODE BEGIN USART2_Init 1 */
+
+    /* USER CODE END USART2_Init 1 */
+    huart2.Instance = USART2;
+    huart2.Init.BaudRate = 115200;
+    huart2.Init.WordLength = UART_WORDLENGTH_8B;
+    huart2.Init.StopBits = UART_STOPBITS_1;
+    huart2.Init.Parity = UART_PARITY_NONE;
+    huart2.Init.Mode = UART_MODE_TX_RX;
+    huart2.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+    huart2.Init.OverSampling = UART_OVERSAMPLING_16;
+    huart2.Init.OneBitSampling = UART_ONE_BIT_SAMPLE_DISABLE;
+    huart2.AdvancedInit.AdvFeatureInit = UART_ADVFEATURE_RXOVERRUNDISABLE_INIT;
+    huart2.AdvancedInit.OverrunDisable = UART_ADVFEATURE_OVERRUN_DISABLE;
+    if (HAL_UART_Init(&huart2) != HAL_OK)
     {
-
-        /* USER CODE BEGIN USART2_Init 0 */
-
-        /* USER CODE END USART2_Init 0 */
-
-        /* USER CODE BEGIN USART2_Init 1 */
-
-        /* USER CODE END USART2_Init 1 */
-        huart2.Instance = USART2;
-        huart2.Init.BaudRate = 115200;
-        huart2.Init.WordLength = UART_WORDLENGTH_8B;
-        huart2.Init.StopBits = UART_STOPBITS_1;
-        huart2.Init.Parity = UART_PARITY_NONE;
-        huart2.Init.Mode = UART_MODE_TX_RX;
-        huart2.Init.HwFlowCtl = UART_HWCONTROL_NONE;
-        huart2.Init.OverSampling = UART_OVERSAMPLING_16;
-        huart2.Init.OneBitSampling = UART_ONE_BIT_SAMPLE_DISABLE;
-        huart2.AdvancedInit.AdvFeatureInit = UART_ADVFEATURE_RXOVERRUNDISABLE_INIT;
-        huart2.AdvancedInit.OverrunDisable = UART_ADVFEATURE_OVERRUN_DISABLE;
-        if (HAL_UART_Init(&huart2) != HAL_OK)
-        {
-            Error_Handler();
-        }
-        /* USER CODE BEGIN USART2_Init 2 */
-
-        /* USER CODE END USART2_Init 2 */
-
+        Error_Handler();
     }
+    /* USER CODE BEGIN USART2_Init 2 */
+
+    /* USER CODE END USART2_Init 2 */
+
+}
 
 /**
   * @brief GPIO Initialization Function
   * @param None
   * @retval None
   */
-    static void MX_GPIO_Init(void)
-    {
+static void MX_GPIO_Init(void)
+{
 
-        /* GPIO Ports Clock Enable */
-        __HAL_RCC_GPIOA_CLK_ENABLE();
+    /* GPIO Ports Clock Enable */
+    __HAL_RCC_GPIOA_CLK_ENABLE();
 
-    }
+}
 
 /* USER CODE BEGIN 4 */
-    int __io_putchar(int ch)
+int __io_putchar(int ch)
+{
+    if (ch == '\n')
     {
-        if (ch == '\n')
-        {
-            uint8_t ch2 = '\r';
-            HAL_UART_Transmit(&huart2, &ch2, 1, HAL_MAX_DELAY);
-        }
-
-        HAL_UART_Transmit(&huart2, (uint8_t *) &ch, 1, HAL_MAX_DELAY);
-        return 1;
+        uint8_t ch2 = '\r';
+        HAL_UART_Transmit(&huart2, &ch2, 1, HAL_MAX_DELAY);
     }
+
+    HAL_UART_Transmit(&huart2, (uint8_t *) &ch, 1, HAL_MAX_DELAY);
+    return 1;
+}
+
+void line_append(uint8_t value)
+{
+    if (value == '\r' || value == '\n') {
+        // odebraliśmy znak końca linii
+        if (line_length > 0) {
+            // jeśli bufor nie jest pusty to dodajemy 0 na końcu linii
+            line_buffer[line_length] = '\0';
+            // przetwarzamy dane
+            printf("Otrzymano: %s\n", line_buffer);
+            // zaczynamy zbieranie danych od nowa
+            line_length = 0;
+        }
+    }
+    else {
+        if (line_length >= LINE_MAX_LENGTH) {
+            // za dużo danych, usuwamy wszystko co odebraliśmy dotychczas
+            line_length = 0;
+        }
+        // dopisujemy wartość do bufora
+        line_buffer[line_length++] = value;
+    }
+}
 /* USER CODE END 4 */
 
 /**
   * @brief  This function is executed in case of error occurrence.
   * @retval None
   */
-    void Error_Handler(void)
+void Error_Handler(void)
+{
+    /* USER CODE BEGIN Error_Handler_Debug */
+    /* User can add his own implementation to report the HAL error return state */
+    __disable_irq();
+    while (1)
     {
-        /* USER CODE BEGIN Error_Handler_Debug */
-        /* User can add his own implementation to report the HAL error return state */
-        __disable_irq();
-        while (1)
-        {
-        }
-        /* USER CODE END Error_Handler_Debug */
     }
+    /* USER CODE END Error_Handler_Debug */
+}
 
 #ifdef  USE_FULL_ASSERT
-    /**
-      * @brief  Reports the name of the source file and the source line number
-      *         where the assert_param error has occurred.
-      * @param  file: pointer to the source file name
-      * @param  line: assert_param error line source number
-      * @retval None
-      */
-    void assert_failed(uint8_t *file, uint32_t line)
-    {
-      /* USER CODE BEGIN 6 */
-      /* User can add his own implementation to report the file name and line number,
-         ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
-      /* USER CODE END 6 */
-    }
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t *file, uint32_t line)
+{
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
+}
 #endif /* USE_FULL_ASSERT */
 
 /************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
